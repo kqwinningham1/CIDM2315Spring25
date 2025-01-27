ed35c6fd7ff7e57e7f17c7a49445960f528ff476
kqwinningham1 <kqwinningham1@buffs.wtamu.edu>
2025-01-26 18:04:36 -0600

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Homework1;
+class Program
+{
+    static void Main (string[] args)
+    {
+        // the code for Homework1
+    double X = 2.5;
+    double Y = 3.3;
+    double Z = 4 * X * X + 3 * Y;
+     Console.WriteLine("Z = 4XX + 3Y");
+      Console.WriteLine($" X = {2.5}, Y = {3.3}, the value of Z is: {4 * X * X + 3 * Y}");
+    }
+}
