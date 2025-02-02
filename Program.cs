8ac8721f9583a408f5a809c400860bdf9e4c03c4
kqwinningham1 <kqwinningham1@buffs.wtamu.edu>
2025-02-01 19:04:14 -0600

    HomeWork2 Pt1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Enter your grade (A, B, C, D, F):");
+            string grade = Console.ReadLine(); 
+            Console.WriteLine(grade);
+
+            if (grade=="A"){
+                Console.WriteLine("GPA 4");
+            }
+            else if (grade=="B"){
+                Console.WriteLine("GPA 3");
+            }
+            else if(grade=="C"){
+                Console.WriteLine("GPA 2");
+            }
+            else if(grade=="D"){
+                Console.WriteLine("GPA 1");
+            }
+            else if(grade=="F"){
+                Console.WriteLine("GPA 0");
+            }
+            
+            else{
+                Console.WriteLine("Wrong letter grade");
+            }
+            
+            
+            
+        }
+    }
+}
