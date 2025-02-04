d9ee607ce9b133f002c65007b7876f1067661a18
kqwinningham1 <kqwinningham1@buffs.wtamu.edu>
2025-02-03 19:57:25 -0600

    Homework 2 Bonus Question

---
+++ HomeWork2Bonus.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {
+        // Bonus question code
+         Console.Write("Enter a year: ");
+        int year = Convert.ToInt16(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine($"{year} is a leap year.");
+        }
+        else
+        {
+            Console.WriteLine($"{year} is not a leap year.");
+        }
+        }
+    }
+}
